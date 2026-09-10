c7c1e25c0f4c29aba1a8a92619399a4709dd6a17
oey135 <b01080126412@gmail.com>
2026-09-10 23:44:03 +0900

    feat: 회원가입 테스트 화면 수정, auth 회원가입 기능 수정
    회원가입 동작.

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,30 +1,33 @@
-//===========================
+// =================================
 //
-//      이메일/비밀번호
-//      회원 관리 기능
+//   회원 인증 관리 (이메일/비밀번호)
 //
-//===========================
+// =================================
 
+// Firebase Authentication 기능 가져오기
 import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
-} from "firebase/auth";
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 import { auth } from "../../app.js";
 
 // 회원가입
 export async function signUp(email, password) {
     try {
+        // 입력값 유효성 검사
         if (email.trim() === "" || password.trim() === "") {
             throw new Error("모든 항목을 입력해주세요.");
         }
 
+        // Firebase 회원가입 요청
         const userCredential = await createUserWithEmailAndPassword(
             auth,
             email,
             password,
         );
 
+        // 생성된 사용자 정보
         const user = userCredential.user;
 
         console.log("회원가입 성공!");
